70fac5bd80f4922616ffc63f26c088b378c0608a
Peter deHaan <peter@deseloper.com>
2019-06-21 00:05:24 -0700

    Format .eslintrc file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,24 @@
-module.exports =  {
-  parser:  '@typescript-eslint/parser',  // Specifies the ESLint parser
+module.exports = {
+  parser: "@typescript-eslint/parser", // Specifies the ESLint parser
   env: {
     es6: true,
     node: true
   },
-  extends:  [
-    'plugin:@typescript-eslint/recommended',
-    "eslint:recommended"  // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+  extends: [
+    // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    "plugin:@typescript-eslint/recommended",
+    "eslint:recommended"
   ],
- parserOptions:  {
-    ecmaVersion:  2018,  // Allows for the parsing of modern ECMAScript features
-    sourceType:  'module',  // Allows for the use of imports
+  parserOptions: {
+    // Allows for the parsing of modern ECMAScript features
+    ecmaVersion: 2018,
+    // Allows for the use of imports
+    sourceType: "module"
   },
-  rules:  {
+  rules: {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/indent": ["error", 2],
     "no-console": "warn"
-  },
+  }
 };
